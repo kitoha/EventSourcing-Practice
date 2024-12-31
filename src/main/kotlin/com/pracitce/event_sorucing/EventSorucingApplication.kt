7e96a3f1e0e367e6dece1a274b98772962eb4997
kitoha <kitoha@daum.net>
2024-12-31 18:53:35 +0900

    first commit

---
+++ src/main/kotlin/com/pracitce/event_sorucing/EventSorucingApplication.kt
@@ -0,0 +1,11 @@
+package com.pracitce.event_sorucing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventSorucingApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventSorucingApplication>(*args)
+}
---
+++ src/test/kotlin/com/pracitce/event_sorucing/EventSorucingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pracitce.event_sorucing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventSorucingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
